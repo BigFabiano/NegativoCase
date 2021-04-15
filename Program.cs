140ceec26b240e9a8c729114740d3fe1ba5d34c2
FabianoEtec <nbscbs@gmail.com>
2021-04-15 19:50:27 -0300

    Finalizado usando Case

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NegativoCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("--- CASO ---");
+
+            Console.Write("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case < 0 :
+                Console.WriteLine("NEGATIVO");
+                break;
+
+                case > 0 :
+                Console.WriteLine("POSITIVO");
+                break;
+
+                case  0 :
+                Console.WriteLine("ZERO");
+                break;
+
+            }
+        Console.WriteLine("Obrigado e volte sempre");
+        }
+    }
+}
